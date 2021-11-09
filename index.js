7cbb18e19a1f1292cce7825a17ae1fd3863101d9
cullen will <cullenwill@cullens-MacBook-Pro.local>
2021-11-09 23:33:28 +0100

    almost done

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+   this.items = []
+   this.length = this.items.length
+   
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length ++
+    this.items.sort((ele1,ele2) => {
+        if (ele1 < ele2) {
+          return -1
+        }
+        else if (ele1 > ele2){
+          return 1
+        }
+        else {
+          return 0
+        }
+        
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return this.items[this.items.length -1]
+    }
+  }
+
+  min() {
+    if (this.length == 0){
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return this.items[0]
+    }
+  }
 
   sum() {}
 
